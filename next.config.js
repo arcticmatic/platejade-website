1b12adb98ec056806e9dbec99f705a25675779da
Your New Username <moderndev55@gmail.com>
2024-05-20 23:46:50 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   webpack: (config, { isServer }) => {
-    // Add a rule to handle mp4, mov, and webm files
-    config.module.rules.push({
-      test: /\.(mp4|mov|webm)$/,
-      type: 'asset/resource',
-    });
+  //  webpack: (config, { isServer }) => {
+  //   // Add a rule to handle mp4, mov, and webm files
+  //   config.module.rules.push({
+  //     test: /\.(mp4|mov|webm)$/,
+  //     type: 'asset/resource',
+  //   });
 
-    // Important: return the modified config
-    return config;
-  },
+  //   // Important: return the modified config
+  //   return config;
+  // },
 };
 
 module.exports = nextConfig;
