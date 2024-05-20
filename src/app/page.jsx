0b41bdeeaa05ea76d85529aaddc78101b847f77a
arcticmatic <155797464+arcticmatic@users.noreply.github.com>
2024-05-20 22:10:19 +0300

    Add files via upload

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,1 +1,26 @@
+import About from "./components/About/About";
+import Features from "./components/Features/Features";
+import Advantages from "./components/Advantages/Advantages";
+import Reviews from "./components/Reviews/Reviews";
+import FAQ from "./components/FAQ/FAQ";
+import Download from "./components/Download/Download";
+import ContactInfo from "./components/ContactInfo/ContactInfo";
+import ContactForm from "./components/ContactForm/ContactForm";
+import css from "./page.module.css";
 
+export default function Home() {
+  return (
+    <>
+      <About />
+      <Features />
+      <Advantages />
+      <Reviews />
+      <FAQ />
+      <Download />
+      <div className={css.contactsThumb}>
+        <ContactInfo />
+        <ContactForm />
+      </div>
+    </>
+  );
+}
