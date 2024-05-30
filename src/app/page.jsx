29213db0bb85c776e1a326c8f490024db45fd280
arntflr <arntflr@gmail.com>
2024-05-30 16:59:06 +0300

    fixes mockups

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -14,7 +14,7 @@ export default function Home() {
       <About />
       <Features />
       <Advantages />
-      <Reviews />
+      {/* <Reviews /> */}
       <FAQ />
       <Download />
       {/* <ContactInfo /> */}
