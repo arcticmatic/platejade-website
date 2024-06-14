bf18b06c9cb5a5407444e6397b214a592df57f23
arntflr <arntflr@gmail.com>
2024-06-14 22:42:54 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   webpack: (config, { isServer }) => {
+  webpack: (config, { isServer }) => {
     // Add a rule to handle mp4, mov, and webm files
     config.module.rules.push({
       test: /\.(mp4|mov|webm)$/,
       type: 'asset/resource',
     });
-    
 
     // Important: return the modified config
     return config;
@@ -15,16 +14,27 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'plate-jade-2024.s3.us-east-2.amazonaws.com', 
+        hostname: 'plate-jade-2024.s3.us-east-2.amazonaws.com',
         port: '',
-        pathname: '/website-files/**', 
+        pathname: '/website-files/**',
       },
     ],
   },
-  
-  //  images: {
-  //   domains: ['plate-jade-2024.s3.us-east-2.amazonaws.com'],
-  // },
+
+  async headers() {
+    return [
+      {
+        // Disable caching for all routes
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, must-revalidate',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
